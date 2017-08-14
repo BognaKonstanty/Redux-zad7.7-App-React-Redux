423b376ddc5730fec897f7ebdbf9cb96fd4be563
Bogna Konstanty <rosiakb@gmail.com>
2017-08-14 11:56:21 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { reducer } from './reducer.js';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import { addComment } from './actions.js';
+import DevTools from './DevTools';
 
 
-const store = createStore(reducer);
+const store = createStore(
+	reducer,
+	DevTools.instrument()
+	);
 
 ReactDOM.render(
   <Provider store={store}>
